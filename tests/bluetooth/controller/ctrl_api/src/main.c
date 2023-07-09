e12b881ea5f86c8c4065cca4da0e952462b77a7c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-07-09 04:24:16 -0400

    spelling: asymmetric
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/bluetooth/controller/ctrl_api/src/main.c
+++ tests/bluetooth/controller/ctrl_api/src/main.c
@@ -295,7 +295,7 @@ ZTEST(public, test_int_pause_resume_data_path)
 	zassert_equal_ptr(node, NULL, "");
 
 
-	/*** #4: Asymetric pause/resume ***/
+	/*** #4: Asymmetric pause/resume ***/
 
 	/* Pause data path */
 	llcp_tx_pause_data(&conn, LLCP_TX_QUEUE_PAUSE_DATA_PHY_UPDATE);
